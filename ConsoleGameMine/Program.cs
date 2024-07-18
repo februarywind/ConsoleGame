ba2dfdf309bd381f2f57041f3181f12f9e2a38e0
februarywind <februarywind0@gmail.com>
2024-07-18 16:47:45 +0900

    ConsoleGame uproad
    뱀게임이랑 지뢰찾기 게임

---
+++ ConsoleGameMine/Program.cs
@@ -0,0 +1,290 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        struct Point
+        {
+            public bool mine;
+            public bool mineVisible;
+            public int mineCount;
+            public bool mineFlag;
+        }
+        static Point[] MineMap = new Point[80];
+
+        struct PlayerXy
+        {
+            public int x, y;
+        }
+        static PlayerXy playerXy = new PlayerXy();
+
+        static bool running = true;
+        static int score = 0;
+
+        static int gameBoardX;
+        static int gameBoardY;
+        static int gameMine;
+
+        static void Main(string[] args)
+        {
+            GameStart();
+
+            while (running)
+            {
+                Arrow(Console.ReadKey(false).Key);
+                scoreCheck();
+            }
+        }
+
+        static void GameStart()
+        {
+            int select;
+            do
+            {
+                Console.WriteLine("방향키로 움직이고 스페이스로 클릭, W로 깃발 꽃기");
+                Console.WriteLine("난이도를 선택 하세요.\n1. 초급\n2. 중급\n3. 상급");
+                int.TryParse(Console.ReadLine(), out select);
+            } while (select < 1 || select > 3);
+            Console.Clear();
+            switch (select)
+            {
+                case 1:
+                    gameBoardX = 9;
+                    gameBoardY = 9;
+                    gameMine = 9;
+                    break;
+                case 2:
+                    gameBoardX = 16;
+                    gameBoardY = 16;
+                    gameMine = 40;
+                    break;
+                case 3:
+                    gameBoardX = 30;
+                    gameBoardY = 16;
+                    gameMine = 99;
+                    break;
+            }
+            Array.Resize(ref MineMap, gameBoardX * gameBoardY);
+            Console.CursorVisible = false;
+
+            playerXy.x = 0;
+            playerXy.y = 0;
+
+            SetMine();
+            PaintMap();
+        }
+
+        static void SetMine()
+        {
+            for (int i = 0; i < gameMine; i++)
+            {
+                int randMine = new Random().Next(0, gameBoardX * gameBoardY);
+                if (!MineMap[randMine].mine)
+                {
+                    MineMap[randMine].mine = true;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            for (int i = 0; i < gameBoardY; i++)
+            {
+                for (int j = 0; j < gameBoardX; j++)
+                {
+                    MineMap[j + (i * gameBoardX)].mineCount = MineCheck(j + (i * gameBoardX), false);
+                }
+            }
+        }
+        static void PaintMap()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            bool PlayerPoint = false;
+            for (int i = 0; i < gameBoardY; i++)
+            {
+                for (int j = 0; j < gameBoardX; j++)
+                {
+                    if (playerXy.x == j && playerXy.y == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        PlayerPoint = true;
+                    }
+                    else
+                    {
+                        PlayerPoint = false;
+                    }
+                    Console.SetCursorPosition(j, i);
+                    if (MineMap[j + (i * gameBoardX)].mineFlag)
+                    {
+                        if (!PlayerPoint)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        }
+                        Console.Write("F ");
+                    }
+                    else if (!MineMap[j + (i * gameBoardX)].mineVisible)
+                    {
+                        Console.Write("?");
+                    }
+                    else if (MineMap[j + (i * gameBoardX)].mine)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("M");
+                    }
+                    else
+                    {
+                        if (!PlayerPoint)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        Console.Write(MineMap[j + (i * gameBoardX)].mineCount);
+                    }
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Arrow(ConsoleKey consoleKey)
+        {
+            switch (consoleKey)
+            {
+                case ConsoleKey.UpArrow:
+                    if (playerXy.y > 0)
+                    {
+                        playerXy.y--;
+                    }
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (playerXy.x > 0)
+                    {
+                        playerXy.x--;
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (playerXy.y < gameBoardY - 1)
+                    {
+                        playerXy.y++;
+                    }
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (playerXy.x < gameBoardX - 1)
+                    {
+                        playerXy.x++;
+                    }
+                    break;
+                case ConsoleKey.Spacebar:
+                    MineMap[playerXy.x + playerXy.y * gameBoardX].mineVisible = true;
+                    if (MineMap[playerXy.x + playerXy.y * gameBoardX].mineCount == 0)
+                    {
+                        MineCheck(playerXy.x + playerXy.y * gameBoardX, true);
+                    }
+                    if (MineMap[playerXy.x + playerXy.y * gameBoardX].mine)
+                    {
+                        running = false;
+                    }
+                    PaintMap();
+                    break;
+                case ConsoleKey.W:
+                    MineMap[playerXy.x + playerXy.y * gameBoardX].mineFlag = !MineMap[playerXy.x + playerXy.y * gameBoardX].mineFlag;
+                    PaintMap();
+                    break;
+            }
+            PaintMap();
+        }
+
+        static int MineCheck(int p, bool zeroCheck)
+        {
+            int mineCount = 0;
+            if (p > gameBoardX - 1 && p % gameBoardX != 0)
+            {
+                if (zeroCheck)
+                {
+                    MineMap[p - gameBoardX - 1].mineVisible = true;
+                }
+                else if (MineMap[p - gameBoardX - 1].mine)
+                    mineCount++;
+            }
+            if (p > gameBoardX - 1)
+            {
+                if (zeroCheck)
+                {
+                    MineMap[p - gameBoardX].mineVisible = true;
+                }
+                else if (MineMap[p - gameBoardX].mine)
+                    mineCount++;
+            }
+            if (p > gameBoardX - 1 && p % gameBoardX != gameBoardX - 1)
+            {
+                if (zeroCheck)
+                {
+                    MineMap[p - gameBoardX + 1].mineVisible = true;
+                }
+                else if (MineMap[p - gameBoardX + 1].mine)
+                    mineCount++;
+            }
+            if (p % gameBoardX != 0)
+            {
+                if (zeroCheck)
+                {
+                    MineMap[p - 1].mineVisible = true;
+                }
+                else if (MineMap[p - 1].mine)
+                    mineCount++;
+            }
+            if (p % gameBoardX != gameBoardX - 1)
+            {
+                if (zeroCheck)
+                {
+                    MineMap[p + 1].mineVisible = true;
+                }
+                else if (MineMap[p + 1].mine)
+                    mineCount++;
+            }
+            if (p % gameBoardX != 0 && p < gameBoardX * gameBoardY - gameBoardX)
+            {
+                if (zeroCheck)
+                {
+                    MineMap[p + gameBoardX - 1].mineVisible = true;
+                }
+                else if (MineMap[p + gameBoardX - 1].mine)
+                    mineCount++;
+            }
+            if (p < gameBoardX * gameBoardY - gameBoardX)
+            {
+                if (zeroCheck)
+                {
+                    MineMap[p + gameBoardX].mineVisible = true;
+                }
+                else if (MineMap[p + gameBoardX].mine)
+                    mineCount++;
+            }
+            if (p < gameBoardX * gameBoardY - gameBoardX && p % gameBoardX != gameBoardX - 1)
+            {
+                if (zeroCheck)
+                {
+                    MineMap[p + gameBoardX + 1].mineVisible = true;
+                }
+                else if (MineMap[p + gameBoardX + 1].mine)
+                    mineCount++;
+            }
+            return mineCount;
+        }
+
+        static void scoreCheck()
+        {
+            int temp = 0;
+            foreach (var item in MineMap)
+            {
+                if (item.mineVisible)
+                {
+                    temp++;
+                }
+            }
+            if (temp == gameBoardX * gameBoardY - gameBoardX)
+            {
+                running = false;
+            }
+        }
+    }
+}
---
+++ ConsoleGamesSnake/Program.cs
@@ -0,0 +1,224 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        public struct Point
+        {
+            public int x;
+            public int y;
+        }
+
+        static Point[] SnakeBody = new Point[5];
+
+        static Point starPoint = new Point();
+
+        static bool running = true;
+
+        static int wasd = 4;
+        
+        static int score = 4;
+
+        static int speed = 200;
+        
+        static void Main(string[] args)
+        {
+            GameStart();
+
+            // 게임루프
+            while (running)
+            {
+                // 0.2초마다
+                Thread.Sleep(speed);
+
+                SnakeMove();
+                SnakePaint();
+
+                PaintMap();
+
+                // 입력 있을때만 ReadKey실행
+                if (Console.KeyAvailable)
+                {
+                    Wasd();
+                }
+            }
+        }
+
+        static void PaintMap()
+        {
+            // 윗면, 아랫면 그리기
+            for (int i = 0; i < 40; i += 2)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("□");
+                Console.SetCursorPosition(i, 19);
+                Console.Write("□");
+            }
+            // 옆면 그리기
+            for (int i = 0; i < 20; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.WriteLine("□");
+                Console.SetCursorPosition(40, i);
+                Console.WriteLine("□");
+            }
+        }
+
+        static void Wasd()
+        {
+            ConsoleKey a = Console.ReadKey(false).Key;
+            switch (a)
+            {
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                    wasd = 1;
+                    break;
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                    wasd = 2;
+                    break;
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+                    wasd = 3;
+                    break;
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    wasd = 4;
+                    break;
+                case ConsoleKey.Spacebar:
+                    if (speed == 200)
+                    {
+                        speed = 100;
+                    }
+                    else
+                    {
+                        speed = 200;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void SnakeMove()
+        {
+            // 뱀 몸통 배열의 복사본 생성
+            Point[] tempBody = new Point[SnakeBody.Length];
+            for (int i = 0; i < SnakeBody.Length; i++)
+            {
+                tempBody[i].x = SnakeBody[i].x;
+                tempBody[i].y = SnakeBody[i].y;
+            }
+
+            // 뱀 꼬리 텍스트 삭제
+            Console.SetCursorPosition(SnakeBody[SnakeBody.Length - 1].x, SnakeBody[SnakeBody.Length - 1].y);
+            Console.WriteLine(" ");
+
+            switch (wasd)
+            {
+                // 모든 배열 값을 한칸씩 밀어서 마지막 배열값을 지우고 움직이는 방향쪽으로 첫번째 배열값을 변경
+                case 1:
+                    for (int i = 0; i < SnakeBody.Length - 1; i++)
+                    {
+                        SnakeBody[i + 1].x = tempBody[i].x;
+                        SnakeBody[i + 1].y = tempBody[i].y;
+                    }
+                    SnakeBody[0].y--;
+                    break;
+                case 2:
+                    for (int i = 0; i < SnakeBody.Length - 1; i++)
+                    {
+                        SnakeBody[i + 1].x = tempBody[i].x;
+                        SnakeBody[i + 1].y = tempBody[i].y;
+                    }
+                    SnakeBody[0].x--;
+                    break;
+                case 3:
+                    for (int i = 0; i < SnakeBody.Length - 1; i++)
+                    {
+                        SnakeBody[i + 1].x = tempBody[i].x;
+                        SnakeBody[i + 1].y = tempBody[i].y;
+                    }
+                    SnakeBody[0].y++;
+                    break;
+                case 4:
+                    for (int i = 0; i < SnakeBody.Length - 1; i++)
+                    {
+                        SnakeBody[i + 1].x = tempBody[i].x;
+                        SnakeBody[i + 1].y = tempBody[i].y;
+                    }
+                    SnakeBody[0].x++;
+                    break;
+            }
+
+            // 레벨업 별을 먹으면 배열 길이를 증가시킨다. 이동하면 자동으로 생성된 배열이 채워진다.
+            foreach (var item in SnakeBody)
+            {
+                if (starPoint.x == item.x && starPoint.y == item.y)
+                {
+                    Console.SetCursorPosition(starPoint.x, starPoint.y);
+                    Console.Write("     ");
+                    LevelUp();
+                    Array.Resize(ref SnakeBody, SnakeBody.Length + 1);
+                }
+            }
+
+            // 뱀이 자신에게 닿으면 게임 종료
+            for (int i = 0; i < SnakeBody.Length; i++)
+            {
+                if (SnakeBody[0].x == SnakeBody[i].x && SnakeBody[0].y == SnakeBody[i].y && i != 0)
+                {
+                    GameOver();
+                }
+            }
+
+            // 뱀이 벽에 부디치면 게임 종료
+            if (SnakeBody[0].x <= 1 || SnakeBody[0].x >= 40 || SnakeBody[0].y < 1 || SnakeBody[0].y >= 19)
+            {
+                GameOver();
+            }
+        }
+
+        static void LevelUp()
+        {
+            score++;
+            starPoint.x = new Random().Next(2, 39);
+            starPoint.y = new Random().Next(1, 19);
+            Console.SetCursorPosition(starPoint.x, starPoint.y);
+            Console.Write("L");
+        }
+        static void SnakePaint()
+        {
+            foreach (var item in SnakeBody)
+            {
+                Console.SetCursorPosition(item.x, item.y);
+                Console.WriteLine("S");
+            }
+        }
+    
+        static void GameOver()
+        {
+            running = false;
+            Console.SetCursorPosition(50, 6);
+            Console.WriteLine($"게임 오버 {score}점 입니다.");
+        }
+        static void GameStart()
+        {
+            // 커서 안보이게
+            Console.CursorVisible = false;
+
+            // 시작 뱀 설정
+            for (int i = 0; i < 5; i++)
+            {
+                SnakeBody[i].x = 4 - i;
+                SnakeBody[i].y = 10;
+            }
+
+            // 레벨업 별 생성
+            LevelUp();
+
+            Console.SetCursorPosition(50, 5);
+            Console.WriteLine($"wasd로 움직이고 스페이스 토글로 속도가 두배라니.");
+        }
+    }
+}
